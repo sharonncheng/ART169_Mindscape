9b974fc41da8fbc301dc213a77f82d77e0f82e2a
Sharon Cheng <scsharon@stanford.edu>
2022-10-18 09:50:16 -0700

    imported fantasy environment and camera setup for main scene

---
+++ Mindscape/Assets/_Scripts/SphereController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script to manage sphere movement for the "full" portion of the experience.
+public class SphereController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
